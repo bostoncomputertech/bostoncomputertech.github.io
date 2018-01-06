6e56c90a0a64a794e69cfe59104097d95091e454
bostoncomputertech <35162096+bostoncomputertech@users.noreply.github.com>
2018-01-06 02:21:44 -0500

    First Website Upload

---
+++ Pricing_files/Pricing.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20100930
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return true;}
+function onPageLoad()
+{loadMozillaCSS('Pricing_files/PricingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
